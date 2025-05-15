95f83fecf07ede7a444114e3023175142172d5a0
Zcr4zy <carlinhoscunhaig@gmail.com>
2025-05-15 20:43:52 -0300

    Continuação

--- GranVeiculos/Data/AppDbContext.cs
+++ GranVeiculos/Data/AppDbContext.cs
@@ -2,12 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GranVeiculos.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace GranVeiculos.Data
 {
     public class AppDbContext : DbContext
     {
-        
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Cor> Cores { get; set; }
+        public DbSet<Marca> Marcas { get; set; }
+        public DbSet<Modelo> Modelos { get; set; }
+        public DbSet<Veiculo> Veiculos { get; set; }
     }
 }
--- GranVeiculos/Program.cs
+++ GranVeiculos/Program.cs
@@ -1,12 +1,35 @@
+using GranVeiculos.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+string conexao = builder.Configuration.GetConnectionString("defaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySQL(conexao)
+);
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
 
+// builder.Services.AddSwaggerGen(c =>
+//     c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+//     {
+//         Title = "",
+//         Version = "v1",
+//         Description = "Documentação da API"
+//     });
+// );
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await dbContext.Database.EnsureCreatedAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
